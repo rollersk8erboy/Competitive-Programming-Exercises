47a9e89cead4cf5d7fd85849a3f755ea8d6d1f3e
rollersk8erboy <riveragonzalez@workmail.com>
2025-09-07 22:14:01 -0600

    .gitignore updated

--- Run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CLASS=PalindromeChecker
-if [ -f "$CLASS.java" ]; then javac "$CLASS.java"; fi
-if [ -f "$CLASS.java" ]; then java "$CLASS.java"; fi
-if [ -f "$CLASS.class" ]; then rm -rf "$CLASS.class"; fi
